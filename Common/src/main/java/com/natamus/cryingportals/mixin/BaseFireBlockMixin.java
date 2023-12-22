b6cc724ce78569d26d4100fa2ad42f7f8fc2ec39
Rick South <rick@natamus.com>
2023-12-22 18:22:53 +0100

    (v2.7) Moved mixin files to the Common source folder to make future development easier.

--- Common/src/main/java/com/natamus/cryingportals/mixin/BaseFireBlockMixin.java
+++ Common/src/main/java/com/natamus/cryingportals/mixin/BaseFireBlockMixin.java
@@ -1,4 +1,4 @@
-package com.natamus.cryingportals.fabric.mixin;
+package com.natamus.cryingportals.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
--- Common/src/main/java/com/natamus/cryingportals/util/Reference.java
+++ Common/src/main/java/com/natamus/cryingportals/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.cryingportals.util;
 public class Reference {
 	public static final String MOD_ID = "cryingportals";
 	public static final String NAME = "Crying Portals";
-	public static final String VERSION = "2.6";
+	public static final String VERSION = "2.7";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
