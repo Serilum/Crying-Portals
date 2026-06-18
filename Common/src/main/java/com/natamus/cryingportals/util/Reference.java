2b550c28b0904362148d797f6167f4b01ad7f64f
Rick South <rick@natamus.com>
2026-06-19 00:03:40 +0200

    Updated Crying Portals to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/cryingportals/util/Reference.java
+++ Common/src/main/java/com/natamus/cryingportals/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "cryingportals";
 	public static final String NAME = "Crying Portals";
 	public static final String VERSION = "2.9";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
